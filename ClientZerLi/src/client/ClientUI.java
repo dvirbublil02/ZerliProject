b93dfb1f9fa7f24023264444ad3d240d5f084ee3
Omri <shalevomri10@gmail.com>
2022-05-10 19:56:38 +0300

    fix

--- ClientZerLi/src/client/ClientUI.java
+++ ClientZerLi/src/client/ClientUI.java
@@ -1,32 +1,27 @@
 package client;
-import client_gui.BranchManagerPageController;
+
 import client_gui.ConnectToServerController;
-import entities_users.BranchManager;
 import javafx.application.Application;
 import javafx.stage.Stage;
-/**In this class we starting the first user screen (connecting to the server)
+
+/**
+ * In this class we starting the first user screen (connecting to the server)
  * 
  *
  */
 public class ClientUI extends Application {
-	public static ClientController chat; //only one instance
+	public static ClientController chat; // only one instance
 
-	public static void main( String args[] ) throws Exception
-	   { 
-		    launch(args);  
-	   } // end main
-	 
+	public static void main(String args[]) throws Exception {
+		launch(args);
+	} // end main
+
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		
-	//creating instance of ConnectToServerController then we lunch it.
-		//ConnectToServerController ConnectToServer=new ConnectToServerController();
-		// ConnectToServer.start(primaryStage);
-		BranchManagerPageController branchManager = new BranchManagerPageController();
-		branchManager.start(primaryStage);
-		 
-		
+
+		// creating instance of ConnectToServerController then we lunch it.
+		ConnectToServerController ConnectToServer = new ConnectToServerController();
+		ConnectToServer.start(primaryStage);
 	}
-	
-	
+
 }
