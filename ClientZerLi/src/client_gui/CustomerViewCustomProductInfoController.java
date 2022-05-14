7dafe90c40adb143a70e9f3cce9c7db934875b8b
Mogli-Place <Mogli-Place@DESKTOP-RT5N10D>
2022-05-14 18:36:31 +0300

    button awt mistake change to javafx

--- ClientZerLi/src/client_gui/CustomerViewCustomProductInfoController.java
+++ ClientZerLi/src/client_gui/CustomerViewCustomProductInfoController.java
@@ -1,6 +1,5 @@
 package client_gui;
 
-import java.awt.Label;
 import java.io.IOException;
 import java.net.URL;
 import java.util.NoSuchElementException;
@@ -18,6 +17,7 @@ import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
